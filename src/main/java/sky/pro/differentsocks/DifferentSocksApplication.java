f215941c33de74f0dfcc797644a7761fe5d2ed9d
svetlanajJd <artlanago@gmail.com>
2023-04-08 17:36:46 +0300

    первый коммит

---
+++ src/main/java/sky/pro/differentsocks/DifferentSocksApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.differentsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferentSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DifferentSocksApplication.class, args);
+    }
+
+}
